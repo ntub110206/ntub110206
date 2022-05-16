612a4c242ea009d57c197ebb75e5053c40a956f0
Tony900602 <tony900602@gmail.com>
2022-05-16 16:04:37 +0800

    Tesseract_upload02

--- tesseract_test.py
+++ tesseract_test.py
@@ -1,13 +1,18 @@
 from PIL import Image
 import pytesseract
 import tkinter as tk
+from tkinter import filedialog
 
 ui = tk.Tk()
 ui.title('test')
 
-img_name = './images/test02.png'
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+print(file_path)
+
+img_name = file_path
 img = Image.open(img_name)
 text = pytesseract.image_to_string(img, lang='chi_tra+eng')
 line = text.split('\n')
-str = line[10].split(':')
-print(str[1])
+print(line)
