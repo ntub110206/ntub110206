3db19d478cf9d673a9de4387b181d0b15aaec370
Tony900602 <tony900602@gmail.com>
2022-05-16 22:37:50 +0800

    Tesseract_upload03

--- tesseract_test.py
+++ tesseract_test.py
@@ -2,17 +2,20 @@ from PIL import Image
 import pytesseract
 import tkinter as tk
 from tkinter import filedialog
+import re
 
-ui = tk.Tk()
-ui.title('test')
-
-root = tk.Tk()
-root.withdraw()
+def tesseract(filename):
+    img_name = file_path
+    img = Image.open(img_name)
+    text = pytesseract.image_to_string(img, lang='chi_tra+eng')
+    cost = re.search(r'總計\s*:\s*(\d+)', text)
+    result = cost.group(0)
+    return result
+    
+file = tk.Tk()
+file.withdraw()
 file_path = filedialog.askopenfilename()
-print(file_path)
+# print(file_path)
+txt = tesseract(file_path)
 
-img_name = file_path
-img = Image.open(img_name)
-text = pytesseract.image_to_string(img, lang='chi_tra+eng')
-line = text.split('\n')
-print(line)
+print(txt)
