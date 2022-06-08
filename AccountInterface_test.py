7b113302985b97f748a7a3fe0ea1a800b2790040
11036044 <11036044@ntub.edu.tw>
2022-06-08 20:45:40 +0800

    AccountInterface_upload08

--- AccountInterface_test.py
+++ AccountInterface_test.py
@@ -17,10 +17,44 @@ connection = mysql.connector.connect(
         database ='myfin'
 )
 cursor = connection.cursor()
-account_ID = 0
-account_ID += 1
 
-def PayCom():#確保支出金額輸入欄只能輸入數字
+accounts_ID = 9 ######
+accounts_ID += 1
+#起始直設為0，1代表支出，2代表收入
+        
+
+def Com():
+        if accounts_Type == 1:
+                Pay = "支出"
+                buffer = 0
+                try:
+                        Money = float(PayMoney.get())
+                except:
+                        messagebox.showwarning('警告','金額請輸入數字')
+                        buffer = 1
+                if buffer == 0:
+                        messagebox.showinfo('成功','已儲存一筆支出')
+                        cursor.execute("insert into `account` values('"+ str(accounts_ID) +"', '"+ Pay +"', '"+ PayItem.get() +"', '"+ PayTradeType.get()+ "', '"+ str(datetime.date.today()) +"', '"+ PayCurrency.get() +"', "+str(Money)+", 'Amy','"+ PayNote.get() +"');")
+                        cursor.close()
+                        connection.commit()
+                        connection.close()
+
+        elif accounts_Type == 2:
+                Income = "收入"
+                buffer = 0
+                try:
+                        Money = float(IncomeMoney.get())
+                except:
+                        messagebox.showwarning('警告','金額請輸入數字')
+                        buffer = 1
+                if buffer == 0:
+                        messagebox.showinfo('成功','已儲存一筆收入')
+                        cursor.execute("insert into `account` values('"+ str(accounts_ID) +"', '"+ Income +"', '"+ IncomeItem.get() +"', '"+ IncomeTradeType.get()+ "', '"+ str(datetime.date.today()) +"', '"+ IncomeCurrency.get() +"', "+str(Money)+", 'Amy','"+ IncomeNote.get() +"');")
+                        cursor.close()
+                        connection.commit()
+                        connection.close()
+
+'''def PayCom():#確保支出金額輸入欄只能輸入數字
         Pay = "支出"
         buffer = 0
         try:
@@ -30,13 +64,12 @@ def PayCom():#確保支出金額輸入欄只能輸入數字
                 buffer = 1
         if buffer == 0:
                 messagebox.showinfo('成功','已儲存一筆支出')
-                cursor.execute("insert into `account` values('"+ str(account_ID) +"', '"+ Pay +"', '"+ PayItem.get() +"', '"+ PayTradeType.get()+ "', '"+ str(datetime.date.today()) +"', '"+ PayCurrency.get() +"', "+str(Money)+", 'Amy',null);")
+                cursor.execute("insert into `account` values('"+ str(accounts_ID) +"', '"+ Pay +"', '"+ PayItem.get() +"', '"+ PayTradeType.get()+ "', '"+ str(datetime.date.today()) +"', '"+ PayCurrency.get() +"', "+str(Money)+", 'Amy',null);")
                 cursor.close()
                 connection.commit()
                 connection.close()
 
 def IncomeCom():#確保收入金額輸入欄只能輸入數字
-        global Income
         Income = "收入"
         buffer = 0
         try:
@@ -45,121 +78,15 @@ def IncomeCom():#確保收入金額輸入欄只能輸入數字
                 messagebox.showwarning('警告','金額請輸入數字')
                 buffer = 1
         if buffer == 0:
-                messagebox.showinfo('成功','已儲存一筆支出')   
-
-def income_event():#按下收入鍵會出現以下
-        global IncomeMoney, IncomeItem, IncomeTradeType, IncomeCurrency, IncomeNote       
-        Moneylbl = tk.Label(#金額標籤
-                window,
-                text = '金額',
-                bg = 'orange',
-                fg = 'black',
-                font = ('Arial', 12),
-                width = 30,
-                height = 2
-        )
-        Moneylbl.grid(column=0, row=3, columnspan=3)
-        
-        IncomeMoney = tk.Entry(#輸入金額
-                window,
-                width = 10
-        )
-        IncomeMoney.grid(column=3, row=3)
-    
-        ItemTypelbl = tk.Label(#類別標籤
-                window,
-                text = '類別',
-                bg = '#59deff',
-                fg = 'black',
-                font = ('Arial', 12),
-                width = 30,
-                height = 2
-        )
-        ItemTypelbl.grid(column=0, row=4, columnspan=3)
-
-        IncomeItem = ttk.Combobox(#選擇收入類別
-                window,
-                values = [
-                        '工資',
-                        '獎金',
-                        '投資',
-                        '其他'],
-                width = 10,
-                state = 'readonly'
-        )
-        IncomeItem.current(0)#將收入類別預設為工資
-        IncomeItem.grid(column=3, row=4)
-
-        TradeTypelbl = tk.Label(#交易方式標籤
-                window,
-                text = '交易方式',
-                bg = 'orange',
-                fg = 'black',
-                font = ('Arial', 12),
-                width = 30,
-                height = 2
-        )
-        TradeTypelbl.grid(column=0, row=5, columnspan=3)
-
-        IncomeTradeType = ttk.Combobox(#選擇交易方式
-                window,
-                values = [
-                        '現金',
-                        '轉帳'],
-                width = 10,
-                state = 'readonly'
-        )
-        IncomeTradeType.current(0)#將交易方式預設為現金
-        IncomeTradeType.grid(column=3, row=5)
-
-        Currencylbl = tk.Label(#貨幣標籤
-                window,
-                text = '貨幣',
-                bg = '#59deff',
-                fg = 'black',
-                font = ('Arial', 12),
-                width = 30,
-                height = 2
-        )
-        Currencylbl.grid(column=0, row=6, columnspan=3)
-
-        IncomeCurrency = ttk.Combobox(#選擇貨幣
-                window,
-                values = [
-                        '新台幣',
-                        '美金',
-                        '人民幣',
-                        '日圓',
-                        '歐元'],
-                width = 10,
-                state = 'readonly'
-        )
-        IncomeCurrency.current(0)#將貨幣預設為新台幣
-        IncomeCurrency.grid(column=3, row=6)
-
-        blank2 = tk.Label(#間隔
-                window,
-                width = 40,
-                height = 2
-        )
-        blank2.grid(column=0, row=7, columnspan=4)
-
-        Notelbl = tk.Label(#記帳備註標籤
-                window,
-                text = '備註',
-                bg = 'white',
-                fg = 'black',
-                font = ('Arial', 12),
-                width = 10,
-                height = 2
-        )
-        Notelbl.grid(column=0, row=8)
-
-        IncomeNote = tk.Entry(window, width = 20)#撰寫記帳備註
-        IncomeNote.grid(column=1, row=8, columnspan=2)        
-
-def pay_event():#按下支出鍵會出現以下  
-        global camera, PayMoney, PayItem, PayTradeType, PayCurrency, PayNote
+                messagebox.showinfo('成功','已儲存一筆支出')
+                cursor.execute("insert into `account` values('"+ str(accounts_ID) +"', '"+ Income +"', '"+ IncomeItem.get() +"', '"+ IncomeTradeType.get()+ "', '"+ str(datetime.date.today()) +"', '"+ IncomeCurrency.get() +"', "+str(Money)+", 'Amy',null);")
+                cursor.close()
+                connection.commit()
+                connection.close()'''  
+ 
+def pay_event():#按下支出鍵會出現以下
+        global camera, PayMoney, PayItem, PayTradeType, PayCurrency, PayNote, accounts_Type
+        accounts_Type = 1
         camera = tk.Button(#開啟文字識別
                 window,
                 text = '文字識別',
@@ -286,6 +213,127 @@ def pay_event():#按下支出鍵會出現以下
 
         PayNote = tk.Entry(window, width = 20)#撰寫記帳備註
         PayNote.grid(column=1, row=8, columnspan=2)
+
+def income_event():#按下收入鍵會出現以下
+        global IncomeMoney, IncomeItem, IncomeTradeType, IncomeCurrency, IncomeNote, accounts_Type
+        accounts_Type = 2
+        blank3 = tk.Label(#開啟文字識別
+                window,
+                width = 40,
+                height = 2
+        )
+        blank3.grid(column=1, row=2, columnspan=4)
+
+        Moneylbl = tk.Label(#金額標籤
+                window,
+                text = '金額',
+                bg = 'orange',
+                fg = 'black',
+                font = ('Arial', 12),
+                width = 30,
+                height = 2
+        )
+        Moneylbl.grid(column=0, row=3, columnspan=3)
+        
+        IncomeMoney = tk.Entry(#輸入金額
+                window,
+                width = 10
+        )
+        IncomeMoney.grid(column=3, row=3)
+    
+        ItemTypelbl = tk.Label(#類別標籤
+                window,
+                text = '類別',
+                bg = '#59deff',
+                fg = 'black',
+                font = ('Arial', 12),
+                width = 30,
+                height = 2
+        )
+        ItemTypelbl.grid(column=0, row=4, columnspan=3)
+
+        IncomeItem = ttk.Combobox(#選擇收入類別
+                window,
+                values = [
+                        '工資',
+                        '獎金',
+                        '投資',
+                        '其他'],
+                width = 10,
+                state = 'readonly'
+        )
+        IncomeItem.current(0)#將收入類別預設為工資
+        IncomeItem.grid(column=3, row=4)
+
+        TradeTypelbl = tk.Label(#交易方式標籤
+                window,
+                text = '交易方式',
+                bg = 'orange',
+                fg = 'black',
+                font = ('Arial', 12),
+                width = 30,
+                height = 2
+        )
+        TradeTypelbl.grid(column=0, row=5, columnspan=3)
+
+        IncomeTradeType = ttk.Combobox(#選擇交易方式
+                window,
+                values = [
+                        '現金',
+                        '轉帳'],
+                width = 10,
+                state = 'readonly'
+        )
+        IncomeTradeType.current(0)#將交易方式預設為現金
+        IncomeTradeType.grid(column=3, row=5)
+
+        Currencylbl = tk.Label(#貨幣標籤
+                window,
+                text = '貨幣',
+                bg = '#59deff',
+                fg = 'black',
+                font = ('Arial', 12),
+                width = 30,
+                height = 2
+        )
+        Currencylbl.grid(column=0, row=6, columnspan=3)
+
+        IncomeCurrency = ttk.Combobox(#選擇貨幣
+                window,
+                values = [
+                        '新台幣',
+                        '美金',
+                        '人民幣',
+                        '日圓',
+                        '歐元'],
+                width = 10,
+                state = 'readonly'
+        )
+        IncomeCurrency.current(0)#將貨幣預設為新台幣
+        IncomeCurrency.grid(column=3, row=6)
+
+        blank2 = tk.Label(#間隔
+                window,
+                width = 40,
+                height = 2
+        )
+        blank2.grid(column=0, row=7, columnspan=4)
+
+        Notelbl = tk.Label(#記帳備註標籤
+                window,
+                text = '備註',
+                bg = 'white',
+                fg = 'black',
+                font = ('Arial', 12),
+                width = 10,
+                height = 2
+        )
+        Notelbl.grid(column=0, row=8)
+
+        IncomeNote = tk.Entry(window, width = 20)#撰寫記帳備註
+        IncomeNote.grid(column=1, row=8, columnspan=2)        
+
+
 ##################################################
 
 window = tk.Tk()
@@ -322,7 +370,7 @@ income = tk.Button(#收入鍵
         font = ('Arial', 12),
         width = 10,
         height = 2,
-        command = lambda: [income_event(), camera.grid_forget()]
+        command = income_event
 )#camera.grid_forget():在記錄收入時隱藏文字識別按鈕
 income.grid(column=2, row=0)
 
@@ -334,7 +382,7 @@ save = tk.Button(#儲存鍵
         font = ('Arial', 12),
         width = 10,
         height = 2,
-        command = lambda: [PayCom()]#IncomeCom()
+        command = Com #IncomeCom()
 )
 save.grid(column=3, row=0)
 
