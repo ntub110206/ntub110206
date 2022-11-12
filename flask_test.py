74ac748f1c363c7027fecb974912a76ea2a7a512
Tony900602 <tony900602@gmail.com>
2022-11-12 15:39:47 +0800

    flask_test13

--- flask_test.py
+++ flask_test.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore
 from flask import Flask,request
 
 app = Flask(__name__)
-#按播放鍵執行程式
+# 按播放鍵執行程式
 @app.route("/")
 def hello():
     return "Hello friend!"
@@ -19,24 +19,24 @@ def budgetAdd():
     # 初始化firestore
     db = firestore.client()
 
-    #取得前端資訊(uid,帳目總價,交易項目)
+    # 取得前端資訊(uid,帳目總價,交易項目)
     uid = request.values['uid']
     money = int(request.values['money'])
     title = request.values['payType']
-    print(title)
+    # print(title)
     # 取得資產總額
     doc_budgetRef = db.collection("users").document(uid)
     doc_snap = doc_budgetRef.get()
     budget = int(doc_snap.get('budget'))
-    #計算記帳後資產
+    # 計算記帳後資產
     if title == "額外收入":
         budget += money
     else:
         budget -= money
-    #更新資料庫
+    # 更新資料庫
     doc = {'budget':budget}
     doc_budgetRef.update(doc)
-    #回傳至前端
+    # 回傳至前端
     return str(budget)
 
 @app.route("/budgetDel",methods=['POST'])
@@ -56,12 +56,12 @@ def budgetDel():
     doc_budgetRef = db.collection("users").document(uid)
     doc_Bsnap = doc_budgetRef.get()
     budget = int(doc_Bsnap.get('budget'))
-    #計算帳物刪除後資產
+    # 計算帳物刪除後資產
     if title == "額外收入":
         budget -= int(money)
     else:
         budget += int(money)
-    #更新資料庫
+    # 更新資料庫
     doc = {'budget':budget}
     doc_budgetRef.update(doc)
 
@@ -86,11 +86,11 @@ def budgetUpdate():
     beforeMoney = int(doc_Asnap.get('money'))
     # 取得更改前帳目大項
     beforeTitle = doc_Asnap.get('tradeType')
-    print(beforeTitle)
+    # print(beforeTitle)
     # 取得前端資訊(更改後帳目金額,更改後帳目大項)
     afterMoney = int(request.values['money'])
     afterTitle = request.values['title']
-    print(afterTitle)
+    # print(afterTitle)
     # 取得資產總額
     doc_budgetRef = db.collection("users").document(uid)
     doc_Bsnap = doc_budgetRef.get()
@@ -115,7 +115,7 @@ def budgetUpdate():
         elif beforeMoney < afterMoney:
             money = afterMoney - beforeMoney
             budget -= money
-    #更新資料庫
+    # 更新資料庫
     doc = {'budget':budget}
     doc_budgetRef.update(doc)
 
@@ -139,7 +139,7 @@ def add():
     tradeType = request.values['title']
     detail = request.values['detail']
     money = int(request.values['money'])
-    #格式
+    # 格式
     doc = {
         'date':date,
         'year':year,
@@ -156,15 +156,15 @@ def add():
     doc_budgetRef = db.collection("users").document(uid)
     doc_snap = doc_budgetRef.get()
     budget = int(doc_snap.get('budget'))
-    #計算記帳後資產
+    # 計算記帳後資產
     if tradeType == "額外收入":
         budget += money
     else:
         budget -= money
-    #更新資料庫
+    # 更新資料庫
     doc = {'budget':budget}
     doc_budgetRef.update(doc)
-    #回傳至前端
+    # 回傳至前端
     return f'{doc_Asnap[1].id},{budget}'
 
 @app.route("/total",methods=['POST'])
@@ -199,16 +199,18 @@ def total():
         if doc.get('month') == datetime.datetime.now().month:
             selectTotal += doc.to_dict().get('money')
 
-    #計算支出比例
+    # 計算支出比例
     proportion = selectTotal/costTotal*100
 
-    #回傳至前端
+    # 回傳至前端
     return f'{costTotal},{selectTotal},{proportion}'
 
 @app.route("/get",methods=['POST'])
 def get():
     costTotal = 0
-    incomeTotal = 0
+    salaryTotal = 0
+    shoppingTotal = 0
+    target = True
     if not len(firebase_admin._apps):
         # 引用私密金鑰
         cred = credentials.Certificate('./serviceAccount.json')
@@ -216,28 +218,36 @@ def get():
         firebase_admin.initialize_app(cred)
     # 初始化firestore
     db = firestore.client()
-    #取得前端資訊(uid)
+    # 取得前端資訊(uid)
     uid = request.values['uid']
 
-    # 指向
+    # 指向用戶資料
     doc_budgetRef = db.collection("users").document(uid)
     # 取得資產總額
     doc_snap = doc_budgetRef.get()
     budget = int(doc_snap.get('budget'))
 
-    # 指向
+    # 指向帳務資料
     doc_ref = db.collection("users").document(uid).collection("dataArray")
     # 查詢所有文件
     doc_costRef = doc_ref.stream()
-    # 取得支出，並計算該月總支出
+    # 取得支出，並計算該月總支出、總收入
     for doc in doc_costRef:
         if doc.get('tradeType') != "額外收入" and int(doc.get('month')) == datetime.datetime.now().month:
             costTotal += int(doc.get('money'))
         elif doc.get('tradeType') == "額外收入" and int(doc.get('month')) == datetime.datetime.now().month:
-            incomeTotal += int(doc.get('money'))
+            salaryTotal += int(doc.get('money'))
+
+    # 指向願望清單
+
+    # 取得願望清單價錢，計算欲花費之預算
     
-    #回傳至前端
-    return f'{budget},{incomeTotal},{costTotal}'
+
+    # 計算達成目標之剩餘金額
+    targetMoney = shoppingTotal - budget
+
+    # 回傳至前端
+    return f'{budget},{salaryTotal},{costTotal},{shoppingTotal},{targetMoney}'
 
 if __name__ == '__main__':
     app.debug = True
